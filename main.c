34a721933801b6e702fd4f100776718791173e52
William <Williamshilaire32@gmail.com>
2025-03-28 13:42:51 +0100

    Ajout du fichier de test pour _printf

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+int main(void)
+{
+    int len;
+
+    len = _printf("Test simple\n");
+    _printf("Caractère : %c\n", 'A');
+    _printf("Chaîne : %s\n", "Holberton");
+
+    printf("Nombre de caractères imprimés : %d\n", len);
+    return (0);
+}
